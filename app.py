8e3d496413176f3ce64736807386926305e3a11e
Michael Anthony <me@mikeanthony.dev>
2021-12-11 14:16:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -73,52 +73,12 @@ if __name__ == '__main__':
     read_me_file_path = os.path.join(os.getcwd(), read_me_file_name)
     read_me_file = open(read_me_file_path)
     read_me = read_me_file.read()
-    # Import SLAM info markdown files 
-    WIS_file_name = "WIS.md"
-    WIS_file_path = os.path.join(os.getcwd(), WIS_file_name)
-    WIS_file = open(WIS_file_path)
-    WIS = WIS_file.read()
 
-    # page config and actions
+
     if page == "About":
         st.markdown(read_me)
-    elif page == "What is SLAM?":
-        st.markdown(WIS)
-        num_nodes = 10
-        node_coords = [(1,8), (3,10), (6,10), (10,10), (4,8), (8,8), (3,5), (6,5), (10,5), (4,3)]
-        x = [x for x,y in node_coords]
-        y = [y for x,y in node_coords]
-        xs, ys = [0,1,1,2,2,2,3,3,9,9,9], [1,2,6,3,4,7,8,5,6,7,8] # vertices
-        node_labels = ["u[t-1]", "x[t-1]", "x[t]", "x[t+1]", "u[t]", "u[t+1]", "z[t-1]", "z[t]", "z[t+1]", "m"]
-        node_indices = list(range(num_nodes))
-        plot = figure(title='', x_range=(0,11), y_range=(2,11), tools='', toolbar_location=None)
-        plot.axis.visible = False
-        plot.background_fill_color = "beige"
-        plot.background_fill_alpha = 0.5
-        graph = GraphRenderer()
-        graph.node_renderer.data_source.add(node_indices, 'index')
-        graph.node_renderer.data_source.add(Spectral8, 'color')
-        graph.node_renderer.glyph = Circle(radius=0.7, fill_color="color", line_color="color", fill_alpha=0.5, line_alpha=0.5)
-        graph.edge_renderer.glyph = MultiLine(line_color="#000000", line_width=0.7, line_alpha=0.8, line_dash="dashed")
-        graph.edge_renderer.data_source.data = dict(start=xs, end=ys)
-        #graph.edge_renderer.data_source.data = dict(start=[0]*num_nodes, end=node_indices)
-        graph.node_renderer.data_source.data['name'] = node_labels
-        source = ColumnDataSource(data=dict(xs=x, ys=y, names=node_labels))
-        # start of layout code
-        labels = LabelSet(x='xs', y='ys', text='names', level='glyph', x_offset=0, y_offset=0, source=source, render_mode='canvas')
-        graph_layout = dict(zip(node_indices, zip(x, y)))
-        graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
-        plot.renderers.append(graph)
-        selected_circles = plot.circle([1,4,8,3,6,10], [8,8,8,5,5,5])
-        glyphs = selected_circles.glyph
-        glyphs.size = 80
-        glyphs.fill_alpha = 0.5
-        glyphs.line_color = "#060f1c"
-        glyphs.line_dash = [30, 3]
-        glyphs.line_width = 5
-        plot.add_layout(labels)
-        st.write(plot)
-
+        
+    elif page == "Language Models":
         # stateful input feature below
         size = st.text_input("Stateful input for future updates", read_state("size", engine, session_id))
         write_state("size", size, engine, session_id)
@@ -132,11 +92,9 @@ if __name__ == '__main__':
         if not (read_state_df(engine, session_id + "_df").empty):
             df = read_state_df(engine, session_id + "_df")
             st.write(df)
-    elif page == "Active Neural SLAM":
-        st.title("Active Neural SLAM")
+            
+    elif page == "Evaluation":
         st.header("Work in progress")
 
-    if page == "Autonomous Drone Platform":
-        st.title("Autonomous Drone Platform")
-        st.header("Work in progress")
+  
         
